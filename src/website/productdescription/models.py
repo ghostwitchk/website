98e329eede92ee3ccf2a561754013f29eadfa9a5
ashish <ashishmanchanda96@gmail.com>
2018-06-21 02:23:45 +0530

    bhai risk lene ja rha hoo,meri amanat ko sambhal ke rkhna

--- src/website/productdescription/models.py
+++ src/website/productdescription/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -9,6 +10,8 @@ class Whole(models.Model):
     price = models.FloatField()
     photos = models.URLField()
     category = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Ram(models.Model):
     size = models.CharField(max_length=255)
@@ -17,6 +20,8 @@ class Ram(models.Model):
     description = models.TextField()
     manufacturer = models.CharField(max_length=255)
     key = models.ForeignKey(Whole)
+    def __str__(self):
+        return self.key.name
 
 class Processor(models.Model):
     no_of_cores = models.PositiveIntegerField()
@@ -24,15 +29,19 @@ class Processor(models.Model):
     manufacturer = models.CharField(max_length=255)
     description = models.TextField()
     key = models.ForeignKey(Whole)
-
+    def __str__(self):
+        return  self.key.name
 
 class Cart(models.Model):
-    userid = models.PositiveIntegerField()
+    userid = models.ForeignKey(settings.AUTH_USER_MODEL,blank=True)
     quantity = models.PositiveIntegerField()
     date = models.DateTimeField(auto_now=True)
     productid = models.ForeignKey(Whole)
 
     class Meta:
         unique_together = ("userid","productid")
+    def __str__(self):
+        return self.userid.email
+
 
 
